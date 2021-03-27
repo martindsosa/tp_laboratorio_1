6ed06d98705824241f2e0cb812ecfa54c7d3b416
martin <52429631+martindsosa@users.noreply.github.com>
2021-03-27 18:00:53 -0300

    Delete tp1 directory

--- tp1/calculador.h
+++
@@ -1,7 +0,0 @@
-void MenuOpciones();
-int Suma(float numeroUno,float numeroDos);
-int Resta(float numeroUno,float numeroDos);
-int Multiplicacion(float numeroUno,float numeroDos);
-int Division(float numeroUno,float numeroDos);
-int Factorial(float numeroUno);
-
