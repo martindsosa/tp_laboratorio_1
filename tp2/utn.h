15891ef5126903eb0d82b2a58aba010d5092fa55
martin <52429631+martindsosa@users.noreply.github.com>
2021-03-27 18:00:22 -0300

    Delete tp2 directory

--- tp2/utn.h
+++
@@ -1,8 +0,0 @@
-
-int getInt(char [],char [],int,int);
-float getFloat(char [],char [],float,float);
-char getCharSiNo(char [],char [],char,char);
-char getChar(char[],char[],char,char);
-int esPar(char[],char[]);
-int getFactorial(char[],char[],int);
-
