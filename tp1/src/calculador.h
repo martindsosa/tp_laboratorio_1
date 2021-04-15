57a77ecf4d9f9bee385482360a014deefab75606
Martin Sosa <zurdo_martu@hotmail.com>
2021-04-14 22:40:46 -0300

    14-04-2021

---
+++ tp1/src/calculador.h
@@ -0,0 +1,20 @@
+/*
+ * calculador.h
+ *
+ *  Created on: 1 abr. 2021
+ *      Author: Martin Damian Sosa
+ */
+
+#ifndef CALCULADOR_H_
+#define CALCULADOR_H_
+
+int menuOpciones();
+int suma(float numeroUno,float numeroDos);
+int resta(float numeroUno,float numeroDos);
+int multiplicacion(float numeroUno,float numeroDos);
+int division(float numeroUno,float numeroDos);
+int factorial(float numeroUno);
+
+
+
+#endif /* CALCULADOR_H_ */
---
+++ tp1/src/tp1.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : tp1.c
+ Author      : Martin D. Sosa
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculador.h"
+
+int main()
+{
+	setbuf(stdout,NULL);
+    menuOpciones();
+    printf(" Gracias por haber utilizar la calculadora...");
+    return EXIT_SUCCESS;
+}
+
