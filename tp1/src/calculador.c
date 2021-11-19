509442e43d7d2d43792e6344f2fa4f45cac5c272
Martin Sosa <zurdo_martu@hotmail.com>
2021-11-19 07:23:54 -0300

    arreglo de errores

--- tp1/src/calculador.c
+++ tp1/src/calculador.c
@@ -4,17 +4,105 @@
  *  Created on: 23 sep. 2021
  *      Author: martindamian
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculador.h"
 
-#ifndef CALCULADOR_H_
-#define CALCULADOR_H_
+/**\brief Menu de opciones
+*\param float primer numero
+*\param float segundo numero
+*\return int devuelve la opcion elegida
+**/
+int MenuOpciones(float numeroUno,float numeroDos)
+{
+    int opcion;
+    printf("\t\t MENU DE OPCIONES\n");
+    printf("\t\t ----------------\n");
+    printf(" 1- Ingrese el primer numero (A = %2.f) \n",numeroUno);
+    printf(" 2- Ingrese el segundo numero (B = %2.f) \n",numeroDos);
+    printf(" 3- Calcular las operaciones \n");
+    printf(" 4- Informar todos los resultados \n");
+    printf(" 5- Salir \n");
+    scanf("%d",&opcion);
+    return opcion;
+}
+/**\brief Suma dos numeros
+*\param float primer numero
+*\param float segundo numero
+*\return float  resultado de la suma de dos numeros
+**/
+float SumarNumeros(float numeroUno,float numeroDos)
+{
+    float resultado;
+    resultado = numeroUno + numeroDos;
 
-int MenuOpciones(float,float);
-int SumarNumeros(float,float);
-int RestarNumeros(float,float);
-int MultiplicarNumeros(float,float);
-int DividirNumeros(float,float);
-int Factorial(float);
+    return resultado;
+}
+/**\brief Resta dos numeros
+*\param float primer numero
+*\param float segundo numero
+*\return float  resultado de la resta de dos numeros
+**/
+float RestarNumeros(float numeroUno,float numeroDos)
+{
+    float resultado;
+    resultado = numeroUno - numeroDos;
 
+    return resultado;
+}
+/**\brief Multiplica dos numeros
+*\param float primer numero
+*\param float segundo numero
+*\return float  resultado de la multiplicacion de dos numeros
+**/
+float MultiplicarNumeros(float numeroUno,float numeroDos)
+{
+    float resultado;
+    resultado = numeroUno * numeroDos;
 
+    return resultado;
+}
+/**\brief Divide dos numeros
+*\param float primer numero
+*\param float segundo numero
+*\return float el resultado de la division de dos numeros
+**/
+float DividirNumeros(float numeroUno,float numeroDos)
+{
+    float resultado = -999999;
+    if (numeroDos != 0)
+    {
+        resultado = numeroUno / numeroDos;
+    }
 
-#endif /* CALCULADOR_H_ */
+    return resultado;
+}
+/**\brief Resolver el factorial de un numero
+*\param float numero
+*\return long int Devuelve el numero factoreado o -1-> si el numero es negativo.
+**/
+long int Factorial(float numero)
+{
+    int numeroEntero ;
+    int i;
+    long resultado = 1;
+
+    numeroEntero = (int)numero;
+    if(numeroEntero < 0)
+    {
+        resultado = -1;
+    }
+    else if (numeroEntero == 0)
+    {
+        resultado = 1;
+    }
+    else
+    {
+        for(i = 1; i <= numeroEntero; i++)
+        {
+            resultado *= i;
+        }
+    }
+    return resultado;
+}
+
--- tp1/src/tp1.c
+++ tp1/src/tp1.c
@@ -17,8 +17,16 @@ int main()
 	setbuf(stdout,NULL);
 	float numeroUno=0;
     float numeroDos=0;
+    float resultadoSuma;
+    float resultadoResta;
+    float resultadoMulti;
+    float resultadoDivision;
+    long int resultadoFactorialUno;
+    long int resultadoFactorialDos;
+    int numeroUnoInt;
+    int numeroDosInt;
     int operacionElegida;
-
+    int flagCalculo = -1;
 
     do
     {
@@ -34,26 +42,62 @@ int main()
                 scanf("%f",&numeroDos);
                 break;
             case 3:
-                printf("Calculando la SUMA de (%.2f + %.2f)\n",numeroUno,numeroDos);
-                printf("Calculando la RESTA de (%.2f - %.2f)\n",numeroUno,numeroDos);
-                printf("Calculando la MULTIPLICACION de (%.2f * %.2f)\n",numeroUno,numeroDos);
-                printf("Calculando la DIVISION de (%.2f / %.2f)\n",numeroUno,numeroDos);
-                printf("Calculando el factorial de (%.2f !)\n",numeroUno);
-                printf("Calculando el factorial de (%.2f !)\n",numeroDos);
-
-                system("pause");
-                system("cls");
+            	if (numeroUno != 0 || numeroDos != 0){
+            		printf("Calculando la SUMA de (%.2f + %.2f)\n",numeroUno,numeroDos);
+					printf("Calculando la RESTA de (%.2f - %.2f)\n",numeroUno,numeroDos);
+					printf("Calculando la MULTIPLICACION de (%.2f * %.2f)\n",numeroUno,numeroDos);
+					printf("Calculando la DIVISION de (%.2f / %.2f)\n",numeroUno,numeroDos);
+					printf("Calculando el factorial de (%.2f !)\n",numeroUno);
+					printf("Calculando el factorial de (%.2f !)\n",numeroDos);
+					resultadoSuma = SumarNumeros(numeroUno,numeroDos);
+					resultadoResta = RestarNumeros(numeroUno,numeroDos);
+					resultadoMulti = MultiplicarNumeros(numeroUno,numeroDos);
+					resultadoDivision = DividirNumeros(numeroUno,numeroDos);
+					resultadoFactorialUno = Factorial(numeroUno);
+					resultadoFactorialDos = Factorial(numeroDos);
+					flagCalculo = 0;
+            	}
+            	else{
+            		printf ("Para realizar los calculos uno de los operandos debe ser distinto de cero !!!\n");
+            	}
                 break;
             case 4:
             {
-                SumarNumeros(numeroUno,numeroDos);
-                RestarNumeros(numeroUno,numeroDos);
-                MultiplicarNumeros(numeroUno,numeroDos);
-                DividirNumeros(numeroUno,numeroDos);
-                Factorial(numeroUno);
-                Factorial(numeroDos);
-                system("pause");
-                system("cls");
+            	if (numeroUno != 0 || numeroDos != 0){
+            		if (flagCalculo == 0){
+						flagCalculo = -1;
+            			printf("El resultado de la suma es: %.2f \n",resultadoSuma);
+						printf("El resultado de la resta es: %.2f \n",resultadoResta);
+						printf("El resultado de la multiplicacion es: %.2f \n",resultadoMulti);
+						if (resultadoDivision != -999999){
+							printf("El resultado de la division es: %.2f \n",resultadoDivision);
+						}
+						else
+						{
+							printf("El divisor no puede ser 0 \n");
+						}
+						if (resultadoFactorialUno == -1){
+							printf("%.2f, no se puede factorear por ser un numero NEGATIVO \n",numeroUno);
+						}
+						else{
+							numeroUnoInt = (int)numeroUno;
+							printf("El factorial de %d es %ld \n",numeroUnoInt,resultadoFactorialUno);
+						}
+						if (resultadoFactorialDos == -1){
+							printf("%.2f, no se puede factorear por ser un numero NEGATIVO \n",numeroDos);
+						}
+						else{
+							numeroDosInt = (int)numeroDos;
+							printf("El factorial de %d es %ld \n",numeroDosInt,resultadoFactorialDos);
+						}
+            		}
+            		else{
+            			printf("Para informar los calculos primero debe calcularlos (opcion3)\n");
+            		}
+            	}
+            	else{
+            		printf ("Para informar calculos primero debe ingresar operandos !!!\n");
+            	}
                 break;
             }
             case 5:
